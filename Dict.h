9bb5bb9dc4ddc6ca4a000d6cefca4a7f5fdd0a71
dafnee17 <dacebosp@epsa.upv.es>
2024-12-05 01:13:44 +0100

    Añadida interfaz genérica

--- Dict.h
+++ Dict.h
@@ -1,11 +1,23 @@
 #ifndef DICT_H
 #define DICT_H
-#include <string>
 
-template <typename V> 
+#include <string>
+#include <stdexcept> // Para std::runtime_error
+
+template <typename V>
 class Dict {
-    public:
-        // ... aquí los métodos virtuales puros
+public:
+
+    virtual void insert(std::string key, V value) = 0;
+
+    virtual V search(std::string key) = 0;
+
+    virtual V remove(std::string key) = 0;
+
+    virtual int entries() = 0;
+
+    virtual ~Dict() = default;
 };
 
-#endif
+#endif // DICT_H
+
